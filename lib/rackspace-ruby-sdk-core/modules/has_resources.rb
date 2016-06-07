dbc684595703f9ea6e44d2c86fae03402d397830
Matt Darby <matt.darby@rackspace.com>
2016-06-07 13:17:12 -0400

    Load all resources (fixes #5)

--- lib/rackspace-ruby-sdk-core/modules/has_resources.rb
+++ lib/rackspace-ruby-sdk-core/modules/has_resources.rb
@@ -12,8 +12,7 @@ module Peace::HasResources
 
   module ClassMethods
     def has_resource(klass_name)
-      klass = load_resource!(klass_name)
-      name  = service_name.to_sym
+      name = service_name.to_sym
 
       @@resources       ||= {}
       @@resources[name] ||= []
@@ -34,11 +33,6 @@ module Peace::HasResources
     end
 
     private
-
-    def load_resource!(klass_name)
-      base = "#{Dir.pwd}/lib/rackspace/services"
-      require "#{base}/#{service_name}/#{klass_name}.rb"
-    end
 
     def stub_resources!
       return unless defined? @@resources
