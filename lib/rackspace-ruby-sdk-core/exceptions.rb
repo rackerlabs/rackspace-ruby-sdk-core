4bb707e16a8e3c0c86cbe8e26d728959bb6747b3
Matt Darby <matt.darby@rackspace.com>
2016-07-12 15:11:33 -0400

    Better error messages (Fixes #18)

--- lib/rackspace-ruby-sdk-core/exceptions.rb
+++ lib/rackspace-ruby-sdk-core/exceptions.rb
@@ -1,3 +1,11 @@
 module Peace
-  class BadRequest < StandardError; end
+  class BadRequest < StandardError
+    def initialize(msg)
+      msg        = msg.gsub("u'", "'")
+      match      = msg.match(/not allowed \((.*)' were unexpected\)/)
+      additional = match[1].split("', '").sort.join(', ')
+
+      super "These fields were unexpected: #{additional}"
+    end
+  end
 end
