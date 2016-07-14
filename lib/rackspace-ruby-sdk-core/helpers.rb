28365011cc39b355df48e2097b615bfd15943f02
Matt Darby <matt.darby@rackspace.com>
2016-07-14 14:45:22 -0400

    Better #wait_for and #payload_builder

--- lib/rackspace-ruby-sdk-core/helpers.rb
+++ lib/rackspace-ruby-sdk-core/helpers.rb
@@ -1,16 +1,27 @@
 class Peace::Helpers
 
-  def self.save_and_wait_for(obj)
-    obj.save
+  def self.wait_for(obj, state='ACTIVE')
     obj.reload
 
-    while obj.state != "ACTIVE"
-      sleep 10
-      print '.'
+    while obj.state != state
+      sleep 1 if VCR.current_cassette.recording?
       obj.reload
     end
 
     obj
+  end
+
+  def self.payload_builder(namespace, hash)
+    data            = {}
+    data[namespace] = {}
+
+    hash.each do |(k,v)|
+      if v.present?
+        data[namespace].merge!({ "#{k}": v })
+      end
+    end
+
+    data
   end
 
 end
